73188898f259514041559b2182b0f31dce61cc40
Voxi0 <alif200099@gmail.com>
2024-09-19 23:07:14 +0100

    Third time fixing 'install.nix'
    Also refined 'README.md' to provide guidance on how to install NixOS with NixDots installed on it.

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
     <h1>❄️ NixOS Dotfiles ❄️</h1>
 </div>
 <h2 align="center">My Personal NixOS Config. Feel Free to Copy!</h2>
+
+<h3>Installing NixOS with NixDots</h3>
+Just run this command to fetch NixDots and run the installer - <br>
+'git clone https://github.com/Voxi0/NixDots.git && cd NixDots && nix-shell ./install.nix'
--- install.nix
+++ install.nix
@@ -1,34 +1,22 @@
 { pkgs ? import <nixpkgs> {} }: let
-  DISKO_CONFIG="./disko.nix";
-  MOUNT_POINT="/mnt";
+  diskoConfig = ./disko.nix;
 in pkgs.mkShellNoCC {
   shellHook = ''
-    # Run Disko to partition, format, and mount the disk
-    echo "Running Disko to partition, format, and mount the disk..."
-    sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko "$DISKO_CONFIG" || { echo "Disko failed"; exit 1; }
+    # Partition, format and mount disks using Disko - Disk configurations are defined in the 'disko.nix' file
+    echo "Running Disko..."
+    sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko "${diskoConfig}" || { echo "Disko failed"; exit 1; }
 
-    # Generate default NixOS configuration and install NixOS
-    echo "Generating NixOS configuration..."
-    if ! nixos-generate-config --root ${MOUNT_POINT}; then
-      echo "Failed to generate NixOS configuration"; exit 1;
+    # Move NixDots to the new system
+    echo "Installing NixDots to new system..."
+    sudo mkdir -p /mnt/etc/nixos
+    if ! sudo cp -r ./* /mnt/etc/nixos/; then
+      echo "Failed to install NixDots"; exit 1;
     fi
 
+    # Install NixOS
     echo "Installing NixOS..."
-    if ! nixos-install; then
+    if ! sudo nixos-install --flake /mnt/etc/nixos/#neo; then
       echo "NixOS installation failed"; exit 1;
-    fi
-
-    # Move dotfiles to the new system
-    echo "Copying dotfiles to new system..."
-    sudo cp -r ./* ./.git* ${MOUNT_POINT}/etc/nixos/ || { echo "Failed to copy dotfiles"; exit 1; }
-
-    # Chroot into the new system and rebuild NixOS
-    echo "Entering chroot environment and rebuilding NixOS..."
-    if ! nixos-enter "${MOUNT_POINT}" -- bash -c "
-      cd ${MOUNT_POINT}/etc/nixos
-      sudo nix flake update --experimental-features 'nix-command flakes'
-      sudo nixos-rebuild switch --flake /etc/nixos/#neo"; then
-      echo "NixOS rebuild failed"; exit 1;
     fi
 
     # Installation complete
