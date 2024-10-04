19c6698a422b0b4e3dde905ce80b21fa3552e639
Voxi0 <alif200099@gmail.com>
2024-10-04 08:08:42 +0100

    Base

---
+++ modules/home/desktops/hyprland/apps/ags/default.nix
@@ -0,0 +1,27 @@
+{ lib, config, inputs, pkgs, ... }: {
+  # Module options
+  options = {
+    ags.enable = lib.mkEnableOption "Enables AGS";
+  };
+
+  # Configure AGS if it's enabled
+  config = lib.mkIf config.ags.enable {
+    # Import Nix modules
+    imports = [
+      inputs.ags.homeManagerModules.default
+    ];
+
+    # AGS configuration
+    programs.ags = {
+      enable = true;
+      configDir = ./dotfiles; # Leave as "null" if you don't want Home Manager to manage AGS dotfiles
+
+      # Additional packages to add to GJS's runtime
+      extraPackages = with pkgs; [
+        gtksourceview
+        webkitgtk
+        accountsservice
+      ];
+    };
+  };
+}
