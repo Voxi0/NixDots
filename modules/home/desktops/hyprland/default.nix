25ffb53c6f4f8ca01fd63310ef3c693f34be2fb0
Voxi0 <alif200099@gmail.com>
2024-12-09 00:53:19 +0000

    More Playerctl Binds
    Also 2 scripts for 2 of the Playerctl commands. Basically a toggle
    script that also send a notification to indicate currently set value.
    The notification system is ass right now but will improve soon as
    possible. It just works for now.

--- modules/home/desktops/hyprland/default.nix
+++ modules/home/desktops/hyprland/default.nix
@@ -4,15 +4,65 @@
     ./apps
   ];
 
-  # Base packages
-  home.packages = with pkgs; [
-    # Base
-    hyprpolkitagent inputs.swww.packages.${pkgs.system}.swww libnotify
-		libsForQt5.qt5.qtwayland kdePackages.qtwayland qt6Packages.qt6ct
+  # Home
+  home = {
+    # Base/Required packages
+    packages = with pkgs; [
+      # Base
+      hyprpolkitagent inputs.swww.packages.${pkgs.system}.swww libnotify
+      libsForQt5.qt5.qtwayland kdePackages.qtwayland qt6Packages.qt6ct
 
-    # Utilities
-    pamixer brightnessctl grim slurp feh udiskie hyprwall hyprshade
-  ];
+      # Utilities
+      pamixer brightnessctl playerctl grim slurp feh udiskie hyprwall hyprshade
+    ];
+
+    # Scripts for Hyprland
+    file = {
+      # Toggle shuffle
+      ".config/hypr/scripts/playerctl-shuffle-toggle" = {
+        executable = true;
+        text = ''
+          #!/usr/bin/env bash
+
+          # Get the current shuffle state
+          currentState=$(playerctl shuffle)
+
+          if [ "$currentState" = "On" ]; then
+            playerctl shuffle off
+            notify-send "Playerctl Shuffle" "Off"
+          else
+            playerctl shuffle on
+            notify-send "Playerctl Shuffle" "On"
+          fi
+
+          wait $notificationID
+        '';
+      };
+
+      # There's 3 loop modes, this script toggles between each - "None", "Track" and "Playlist"
+      ".config/hypr/scripts/playerctl-loop-toggle" = {
+        executable = true;
+        text = ''
+          #!/usr/bin/env bash
+
+          # Get the current loop state
+          currentState=$(playerctl loop)
+
+          # Cycle through the states: "None", "Track", "Playlist"
+          if [ "$currentState" = "None" ]; then
+              playerctl loop Track
+              notify-send "Playerctl Loop" "Track"
+          elif [ "$currentState" = "Track" ]; then
+              playerctl loop Playlist
+              notify-send "Playerctl Loop" "Playlist"
+          else
+              playerctl loop None
+              notify-send "Playerctl Loop" "None"
+          fi
+        '';
+      };
+    };
+  };
 
   # Playerctl
   services.playerctld = {
