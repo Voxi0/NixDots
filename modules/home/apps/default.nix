d96f9481f6d2a456e59905462217c4fb3cccd733
Voxi0 <alif200099@gmail.com>
2024-12-09 01:18:39 +0000

    Removed Firefox Completely
    Completely removed Firefox to migrate to Floorp. No reason to keep both
    browsers because I can't sync their customizations without giving too
    much time and effort than it's worth.

--- modules/home/apps/default.nix
+++ modules/home/apps/default.nix
@@ -1,13 +1,14 @@
 { lib, ... }: {
   # Import Nix modules
   imports = [
-    ./shell.nix ./editors ./git.nix ./nixcord.nix ./browsers ./obsidian.nix ./spotify.nix
+    ./shell.nix ./editors ./git.nix ./nixcord.nix ./floorp ./obsidian.nix ./spotify.nix
   ];
 
   # Enable all applications by default
   enableZSH = lib.mkDefault true;
   enableKitty = lib.mkDefault true;
   enableGit = lib.mkDefault true;
+  enableFloorp = lib.mkDefault true;
   enableNixcord = lib.mkDefault true;
 	enableObsidian = lib.mkDefault true;
 	enableSpotify = lib.mkDefault true;
