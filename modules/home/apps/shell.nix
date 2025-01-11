ab41327a9d4c7dbe38f9789dd09210ab1bf77062
Voxi0 <alif200099@gmail.com>
2025-01-11 15:08:29 +0000

    Kitty Revamp
    Added some extra configuration for the Kitty terminal. It looks really
    nice now in my opinion. Love the cursor trails :p

--- modules/home/apps/shell.nix
+++ modules/home/apps/shell.nix
@@ -42,14 +42,42 @@
         # Cursor
         cursor_shape = "beam";
         cursor_beam_thickness = "1.0";
-        cursor_blink_interval = 0;
+        cursor_blink_interval = "0";
+        cursor_trail = "3";
+        cursor_trail_decay = "0.1 0.4";
+        cursor_trail_start_threshold = "1";
 
         # Performance tuning
         sync_to_monitor = true;
 
+        # Tab bar position, style and alignment
+        tab_bar_edge = "top";
+        tab_bar_min_tabs = "1";
+        tab_bar_style = "powerline";
+        tab_powerline_style = "slanted";
+        tab_bar_align = "left";
+
+        # When the current tab is closed, go back to the previous tab
+        tab_switch_strategy = "previous";
+
+        # Tab bar title
+        tab_title_max_length = "0";
+        tab_title_template = "{fmt.fg.red}{bell_symbol}{activity_symbol}{fmt.fg.tab}{title}";
+
+        # Active/Inactive tab style
+        active_tab_font_style = "bold";
+        inactive_tab_font_style = "normal";
+
         # Terminal bell
-        enable_audio_bell = true;
+        enable_audio_bell = false;
         window_alert_on_bell = true;
+        
+        # URLs
+        detect_urls = true;
+        url_prefixes = "file ftp ftps gemini git gopher http https irc ircs kitty mailto news sftp ssh";
+        underline_hyperlinks = "always";
+        url_style = "curly";
+        open_url_with = "default";
       };
     };
 
