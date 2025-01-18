0cc37b343685a8411f9e69baa38375eec19110a6
voxi0 <alif200099@gmail.com>
2025-01-18 02:37:00 +0000

    Music Visualizer
    Added support for music visualizer to NCMPCPP.

--- modules/home/apps/music.nix
+++ modules/home/apps/music.nix
@@ -1,4 +1,4 @@
-{ lib, config, inputs, pkgs, ... }: {
+{ lib, config, inputs, pkgs, username, ... }: {
   # Import Nix modules
   imports = [ inputs.spicetify-nix.homeManagerModules.default ];
 
@@ -8,45 +8,58 @@
     enableNcmpcpp = lib.mkEnableOption "Enables NCMPCPP";
   };
 
-	# Configure Spotify if it's enabled
+	# Configure enabled music players
 	config = {
     # Services
     services = lib.mkIf config.enableNcmpcpp {
       # Music Player Daemon (MPD) - A free and open-source music player server (Required for NCMPCPP)
       mpd = {
         enable = true;
-        musicDirectory = "$XDG_MUSIC_DIR";
-        playlistDirectory = "\${dataDir}/playlists";
-        dataDir = "$XDG_DATA_HOME/mpd";
-        dbFile = "\${dataDir}/tag_cache";
+        musicDirectory = "/home/${username}/Music";
+        network = {
+          startWhenNeeded = true;
+          listenAddress = "any";
+        };
         extraArgs = [ ];
         extraConfig = ''
+          # Audio outputs
+          audio_output {
+            type "pipewire"
+            name "PipeWire Output"
+          }
+          audio_output {
+            type "fifo"
+            name "my_fifo"
+            path "/tmp/mpd.fifo"
+            format "44100:16:2"
+          }
         '';
       };
+
+      # Discord rich presence for MPD - Shows what you're listening to etc.
       mpd-discord-rpc = {
         enable = true;
-        settings = {
-          format = {
-            large_image = "notes";
-            large_text = "";
-            small_image = "notes";
-            small_text = "";
-            details = "$title";
-            state = "On $album by $artist";
-            timestamp = "elapsed";
-          };
+        settings.format = {
+          large_image = "notes";
+          large_text = "";
+          small_image = "notes";
+          small_text = "";
+          details = "$title";
+          state = "On $album by $artist";
+          timestamp = "elapsed";
         };
       };
 
       # MPDris2 - MPD to MPRIS2 bridge
       mpdris2 = {
         enable = true;
-        multimediaKeys = false;
+        multimediaKeys = true;
         notifications = false;
       };
     };
 
     # Programs
+    stylix.targets.cava.rainbow.enable = true;
     programs = {
       # Spicetify - Powerful CLI tool to take control of the Spotify client
       spicetify = lib.mkIf config.enableSpotify {
@@ -65,7 +78,24 @@
       # NCMPCPP - Featureful Ncurses based MPD client inspired by NCMPC
       ncmpcpp = {
         enable = true;
-        settings = {};
+        package = pkgs.ncmpcpp.override {
+          outputsSupport = true;
+          taglibSupport = true;
+          clockSupport = true;
+          visualizerSupport = true;
+        };
+        settings = {
+          # Music visualizer
+          visualizer_data_source = "/tmp/mpd.fifo";
+          visualizer_output_name = "my_fifo";
+          visualizer_in_stereo = true;
+          visualizer_type = "spectrum";
+          visualizer_spectrum_smooth_look = true;
+          visualizer_spectrum_smooth_look_legacy_chars = false;
+          visualizer_look = "+|";
+          visualizer_fps = 60;
+          visualizer_autoscale = true;
+        };
         bindings = [
           # VIM navigation
           { key = "j"; command = "scroll_down"; }
