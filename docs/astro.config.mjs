5407a33023a4022bff0ffa181080495e0a6196d4
voxi0 <alif200099@gmail.com>
2025-07-07 23:29:50 +0100

    Trying to Fix Docs Homepage #3

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,6 +5,7 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://voxi0.github.io",
+	base: "NixDots"
 	integrations: [
 		starlight({
 			title: "NixDots",
