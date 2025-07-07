886575f0ab7cdce5ffc1f8dc3f16a230d088bb2f
voxi0 <alif200099@gmail.com>
2025-07-07 22:31:39 +0100

    Removed `docs` and `hardware-configuration.nix`
    I'm confused

--- docs/js/open-links-in-new-tab.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelectorAll('a[href^="http"]').forEach(link => {
-        link.setAttribute('target', '_blank');
-        link.setAttribute('rel', 'noopener noreferrer');
-    });
-});
