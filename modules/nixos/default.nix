d86e3ad17a2ecaf88c08a356de6ec2898a62d134
voxi0 <alif200099@gmail.com>
2025-07-12 23:44:04 +0000

    Modularizing `configuration.nix`
    Modularizing everything in a host's `configuration.nix`. The
    `configuration.nix` is way cleaner now and we're not rewriting the same
    code for multiple hosts.

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,15 +1,68 @@
-_: {
+{ lib, config, locale, pkgs, ... }: {
 	# Import Nix modules
 	imports = [
 		./hardware						# Hardware specific configuration e.g. graphics drivers
 		./stylix.nix					# System-wide theming and typography
+		./desktops/hyprland		# Desktop environment / Window manager
 		./users.nix						# Configure user groups and such
 		./services						# System services
 		./programs.nix				# System programs
-		./android.nix					# Android development tools
 		./virtualization.nix	# Virtualization stuff e.g. KVM (Kernel-based Virtual Machine)
-		./desktops/hyprland		# Desktop environment / Window manager
 		./fish.nix						# Fancy shell
 		./gaming.nix					# Gaming related stuff
+	];
+
+	# Module options
+	options = {
+		enableNetworking = lib.mkEnableOption "Enable networking";
+		enableXdgPortals = lib.mkEnableOption "Enable XDG portals (Service that allows apps to interact with the desktop safely)";
+	};
+
+	# Configuration
+	config = lib.mkMerge [
+		# No conditions used here
+		{
+			# Boot
+			boot = {
+				kernelPackages = pkgs.linuxPackages_latest;
+				kernelParams = [ ];
+				extraModulePackages = with config.boot.kernelPackages; [ ];
+				kernelModules = [ ];
+				loader = {
+					systemd-boot.enable = true;
+					efi.canTouchEfiVariables = true;
+				};
+			};
+
+			# Security
+			security = {
+				polkit.enable = true;
+				rtkit.enable = true;
+			};
+
+			# Internationalisation properties
+			i18n.defaultLocale = locale;
+
+			# Don't change this value
+  		system.stateVersion = "25.05";
+		}
+
+		# Networking
+		(lib.mkIf config.enableNetworking {
+			environment.systemPackages = [ pkgs.networkmanagerapplet ];
+			networking = {
+				hostName = "NixOS-Desktop";
+				networkmanager.enable = true;
+				firewall.enable = true;
+			};
+		})
+
+		# XDG desktop portals
+		(lib.mkIf config.enableXdgPortals {
+			xdg.portal = {
+				enable = true;
+				extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+			};
+		})
 	];
 }
