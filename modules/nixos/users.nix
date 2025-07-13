8ca7fb4522a3ed844c885e3b6122cfa3e9ecc073
voxi0 <alif200099@gmail.com>
2025-07-13 02:13:32 +0100

    Removing Unnecessary Files
    So I'm taking everything inside tiny NixOS modules and putting them in
    `default.nix` instead. It's cleaner and easier to read. The
    `default.nix` file isn't too large or anything so all is well. Now less
    files cluttering the place :)

--- modules/nixos/users.nix
+++
@@ -1,9 +0,0 @@
-{ username, ... }: {
-	# Users - Remember to set a password with `passwd`
-  users.users.${username} = {
-    isNormalUser = true;
-    initialPassword = "nixos";
-    description = "${username}";
-    extraGroups = [ "networkmanager" "wheel" "cdrom" ];
-  };
-}
