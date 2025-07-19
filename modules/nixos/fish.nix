347578d55085ee21f448487e2a6c2d7414a97c58
voxi0 <alif200099@gmail.com>
2025-07-19 02:17:58 +0100

    Added `loginShellInit`
    Now as soon as the user logs into the system, the UWSM menu is opened so
    they can choose what Wayland compositor they would like to use. Display
    Managers are overrated. TTY logins and UWSM menu is all I need.

--- modules/nixos/fish.nix
+++ modules/nixos/fish.nix
@@ -10,21 +10,26 @@
 
   # Configuration
   config = lib.mkIf config.enableFish {
-    # Keep Bash as the system shell but have it start Fish when running interactively
-    # This is because Fish isn't POSIX compliant meaning it could cause issues if used as the login shell
-    programs.bash.interactiveShellInit = ''
-       	# Launch Fish unless the parent process is already Fish
-         if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-         then
-           shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-           exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-         fi
+		# Keep using Bash as the system/login shell
+    programs.bash = {
+			# Bring up UWSM compositor selection menu after logging in
+			loginShellInit = ''
+				if uwsm check may-start && uwsm select; then
+					exec uwsm start default
+				fi
+			'';
 
-      # Bring up UWSM compositor selection menu after logging in
-      if uwsm check may-start && uwsm select; then
-      	exec uwsm start default
-      fi
-    '';
+			# Start Fish when running interactively
+    	# This is because Fish isn't POSIX compliant meaning it could cause issues if used as the login shell
+			interactiveShellInit = ''
+				# Launch Fish unless the parent process is already Fish
+				if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+				then
+					shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+					exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+				fi
+    	'';
+		};
 
     # Enable vendor Fish completions provided by Nixpkgs
     programs.fish.enable = true;
