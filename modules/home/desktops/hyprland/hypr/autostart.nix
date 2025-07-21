9792d910dff0bca70b2a83241f2074559805c190
voxi0 <alif200099@gmail.com>
2025-07-22 00:14:28 +0100

    Fixed `autostart.nix`
    Whoops errors

--- modules/home/desktops/hyprland/hypr/autostart.nix
+++ modules/home/desktops/hyprland/hypr/autostart.nix
@@ -1,18 +1,17 @@
 {
   lib,
+  config,
   pkgs,
   ...
 }: {
   #################
   ### AUTOSTART ###
   #################
-  wayland.windowManager.hyprland.settings.exec-once = lib.concatLists [
-		(lib.mkIf (pkgs ? mpdscribble) [ "uwsm app -- mpdscribble" ])
-		[
-			"uwsm app -- quickshell"
-			"uwsm app -- swww-daemon"
-			"swww restore"
-			"uwsm app -- swaync"
-		]
+  wayland.windowManager.hyprland.settings.exec-once = [
+    "uwsm app -- quickshell"
+    "uwsm app -- swww-daemon"
+    "swww restore"
+    "uwsm app -- swaync"
+    (lib.optionals (pkgs ? mpdscribble) "uwsm app -- mpdscribble")
   ];
 }
