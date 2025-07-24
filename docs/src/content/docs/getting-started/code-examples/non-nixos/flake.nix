dfd0dabeecd6f9b32359ea001f416e27cbe08bc3
voxi0 <alif200099@gmail.com>
2025-07-24 01:14:52 +0100

    Fixing Code Examples
    And also formatting.

--- docs/src/content/docs/getting-started/code-examples/non-nixos/flake.nix
+++ docs/src/content/docs/getting-started/code-examples/non-nixos/flake.nix
@@ -1,62 +1,64 @@
-description = "Standalone Home Manager config on Non-NixOS machine that uses NixDots";
-inputs = {
-	NixDots = {
-		url = "github:Voxi0/NixDots";
+{
+  description = "Standalone Home Manager config on Non-NixOS machine that uses NixDots";
+  inputs = {
+    NixDots = {
+      url = "github:Voxi0/NixDots";
 
-		# If you define your own Nixpkgs, this also applies to all other NixDots inputs e.g. Home Manager
-		# inputs.nixpkgs.follows = "nixpkgs";
-	};
-};
-outputs = {...}@inputs: let
-	system = "x86_64-linux";
+      # If you define your own Nixpkgs, this also applies to all other NixDots inputs e.g. Home Manager
+      # inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = _: let
+    system = "x86_64-linux";
 
-	# We're using the Nixpkgs defined in NixDots but you can use your own Nixpkgs if you wish
-	pkgs = import inputs.NixDots.inputs.nixpkgs {
-		inherit \${system};
-		config.allowUnfree = true;
-	};
+    # We're using the Nixpkgs defined in NixDots but you can use your own Nixpkgs if you wish
+    pkgs = import inputs.NixDots.inputs.nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
 
-	# Just for convenience
-	nixdotsHomeModules = inputs.NixDots.homeManagerModules;
+    # Just for convenience
+    nixdotsHomeModules = inputs.NixDots.homeManagerModules;
 
-	# REQUIRED FOR NIXDOTS TO WORK
-	username = "your-username";
-	kbLayout = "us";
-in {
-	homeConfigurations.\${username} = inputs.NixDots.inputs.home-manager.lib.homeManagerConfiguration {
-		inherit pkgs;
-		extraSpecialArgs = {
-			# So you can avoid having to define any inputs that NixDots requires, using it's own inputs instead
-			inputs = inputs.NixDots.inputs;
+    # REQUIRED FOR NIXDOTS TO WORK
+    username = "your-username";
+    kbLayout = "us";
+  in {
+    homeConfigurations.${username} = inputs.NixDots.inputs.home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      extraSpecialArgs = {
+        # So you can avoid having to define any inputs that NixDots requires, using it's own inputs instead
+        inherit (inputs.NixDots) inputs;
 
-			# REQUIRED FOR NIXDOTS TO WORK
-			inherit system username kbLayout;
-		};
-		modules = [
-			# Your Home Manager config
-			./home.nix
+        # REQUIRED FOR NIXDOTS TO WORK
+        inherit system username kbLayout;
+      };
+      modules = [
+        # Your Home Manager config
+        ./home.nix
 
-			# Choose whatever modules you desire
-			# Every single module
-			nixdotsHomeModules.default
+        # Choose whatever modules you desire
+        # Every single module
+        nixdotsHomeModules.default
 
-			# Window manager / Desktop environments and theming/styling
-			nixdotsHomeModules.desktops
-			nixdotsHomeModules.stylix
+        # Window manager / Desktop environments and theming/styling
+        nixdotsHomeModules.desktops
+        nixdotsHomeModules.stylix
 
-			# My wallpaper collection
-			nixdotsHomeModules.wallpapers
+        # My wallpaper collection
+        nixdotsHomeModules.wallpapers
 
-			# CLI stuff
-			nixdotsHomeModules.fish
-			nixdotsHomeModules.cli
+        # CLI stuff
+        nixdotsHomeModules.fish
+        nixdotsHomeModules.cli
 
-			# Apps
-			nixdotsHomeModules.apps.default
-			nixdotsHomeModules.apps.kitty
-			nixdotsHomeModules.apps.browser
-			nixdotsHomeModules.apps.spotify
-			nixdotsHomeModules.apps.discord
-		];
-	};
-};
+        # Apps
+        nixdotsHomeModules.apps.default
+        nixdotsHomeModules.apps.kitty
+        nixdotsHomeModules.apps.browser
+        nixdotsHomeModules.apps.spotify
+        nixdotsHomeModules.apps.discord
+      ];
+    };
+  };
+}
--- modules/home/cli/default.nix
+++ modules/home/cli/default.nix
@@ -7,8 +7,8 @@
   # Import Nix modules
   imports = [
     ./fastfetch.nix # Modern system info tool designed to replace Neofetch
-		./git.nix	# Git and LazyGIT (Beautiful TUI for Git that makes using Git super fast and easy)
-		./yazi.nix	# Fancy and modern TUI file manager written in Rust with previews and such
+    ./git.nix # Git and LazyGIT (Beautiful TUI for Git that makes using Git super fast and easy)
+    ./yazi.nix # Fancy and modern TUI file manager written in Rust with previews and such
     ./ncmpcpp.nix # MPD client - TUI music player
   ];
 
