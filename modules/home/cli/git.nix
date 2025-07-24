e8a1f9ee280d1c704e8c4aebe1360a1c35d8afbc
voxi0 <alif200099@gmail.com>
2025-07-24 01:19:01 +0100

    Exporting More CLI Modules
    Neat now the user has more flexibility.

--- modules/home/cli/git.nix
+++ modules/home/cli/git.nix
@@ -5,7 +5,7 @@
   ...
 }: {
   # Module options
-  options.enableGit = lib.mkEnableOption "Enable Git - The most popular version control system";
+  options.cli.enableGit = lib.mkEnableOption "Enable Git - The most popular version control system";
 
   # Git and LazyGIT (Beautiful TUI for Git that makes using Git super fast and easy)
   config = lib.mkIf config.cli.enableGit {
